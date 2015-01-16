23e0ae652b3eb3d936ab78feea32593683ad8706
dheerajkumar147@gmail.com <dheerajkumar147@gmail.com@8a863159-9952-5ae7-e03f-e488488e72c2>
2015-01-16 11:49:55 +0000

    Update with some notations and user defined table and columns

--- Hibernate/src/com/javabrain/main/HibernateTest.java
+++ Hibernate/src/com/javabrain/main/HibernateTest.java
@@ -1,5 +1,7 @@
 package com.javabrain.main;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,7 +13,10 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		UserDetails user = new UserDetails();
 		user.setUserId(2);
-		user.setUserName("Secone User");
+		user.setUserName("Dheeraj Kumar");
+		user.setAddress("Sr. No. 207, Sahyog, front of IBM Company, Tukai Darshan, Pune, Maharashtra, India 412308");
+		user.setJoinedDate(new Date());	//this is save the Current date and time
+		user.setDecription("I am working in Techsignia, It's a CMM Level 5 Company");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
