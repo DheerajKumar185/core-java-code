ec848052b511350ea7b5c7195fa630eb516310d0
dheerajkumar147@gmail.com <dheerajkumar147@gmail.com@8a863159-9952-5ae7-e03f-e488488e72c2>
2015-05-06 07:00:52 +0000

    Initial import.

---
+++ trunk/Test/src/org/format/Squire.java
@@ -0,0 +1,25 @@
+package org.format;
+
+public class Squire {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10;i++)
+		{
+			System.out.println("["+"\""+i+"\""+","+"\""+i+"\""+"],");
+		}
+		
+//		for(int i = 1; i <= 10; i++)
+//		{
+//			System.out.print("[");
+//			for(int j = 1; j <= 1; j++)
+//			{
+//				System.out.print("\"" + i + "\"" + ",");
+//				System.out.print("\"" + i + "\"");
+//			}
+//			System.out.print("]");
+//			System.out.println(",");
+//		}
+
+	}
+
+}
