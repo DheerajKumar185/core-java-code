cb6a11a486d61a172fba5c39b6ce331f26bcfdd1
DOK$ <DOK$@DOK-PC>
2016-01-17 09:31:52 +0530

    Add Anonymous Class.

---
+++ Core_Java/src/com/tech/doks/Anonymous.java
@@ -0,0 +1,19 @@
+package com.tech.doks;
+
+public class Anonymous {
+	/*Anonymous simply means nameless.
+	 * An object that have no reference is known as anonymous object.
+	 * if you have to use an object only once, anonymous object is a good approach.*/
+	
+	int length, width;
+	
+	void calculateArea(int x, int y) {
+		length = x;
+		width = y;
+		System.out.println(length * width);
+	}
+	
+	public static void main(String[] args) {
+		new Anonymous().calculateArea(10, 20);
+	}
+}
